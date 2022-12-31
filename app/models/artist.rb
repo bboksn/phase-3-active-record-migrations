36842b42a975a59dbad853bb1e2176dad7ab7c4c
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-12-31 12:25:35 -0500

    commit tree 4fada3fbdf7c4ebd562deddeed890b3c0ee376a6

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,8 @@
 # add the Artist class here
+class Artist < ActiveRecord::Base
+    class AddFavoriteFlowerToArtists < ActiveRecord::Migration[6.1]
+        def change
+          add_column :artists, :favorite_flower, :string
+        end
+      end
+end
